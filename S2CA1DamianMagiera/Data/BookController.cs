00389c4027545286b3083525b5f70d5c7fd06f28
Damian0222 <d00229247@student.dkit.ie>
2025-02-06 22:14:56 +0000

    Sets up a database connection and gets the Authors table.

--- S2CA1DamianMagiera/Data/BookController.cs
+++ S2CA1DamianMagiera/Data/BookController.cs
@@ -1,6 +1,11 @@
-﻿namespace S2CA1DamianMagiera.Data
+﻿using S2CA1DamianMagiera.Models;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+namespace S2CA1DamianMagiera.Data
 {
-    public class BookController
+    public class BookstoreContext : DbContext
     {
-    }
+        public BookstoreContext(DbContextOptions<BookstoreContext> options) : base(options) { }
+        public DbSet<Author> Authors { get; set; }
+         }
 }
