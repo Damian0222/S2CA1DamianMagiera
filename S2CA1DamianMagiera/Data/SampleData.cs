54effdfe4535b9fbb587f3f935912ad245890807
Damian0222 <d00229247@student.dkit.ie>
2025-02-07 15:56:48 +0000

    Added sample data

--- S2CA1DamianMagiera/Data/SampleData.cs
+++ S2CA1DamianMagiera/Data/SampleData.cs
@@ -6,15 +6,23 @@ namespace S2CA1DamianMagiera.Data
     {
         public static void Initialize(BookContext context)
         {
-            if (!context.Authors.Any()) 
+            if (!context.Authors.Any())
             {
                 context.Authors.AddRange(
                     new Author { Id = 1, Name = "J.K Rowling", Bio = "Harry Potter Books" },
                     new Author { Id = 2, Name = "George Martin", Bio = "Game of Thrones Books" }
-                );
-            }
 
-            context.SaveChanges();
+
+                    );
+            }
+                    if (!context.Books.Any())
+                {
+                    context.Books.AddRange(
+                        new Book { Id = 1, Title = "Harry Potter and the Sorcerer's Stone", AuthorId = 1 },
+                        new Book { Id = 2, Title = "A Game of Thrones", AuthorId = 2 }
+                    );
+                }
+
+            }
         }
-    }
 }
