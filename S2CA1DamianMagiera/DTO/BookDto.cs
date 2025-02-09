98f8337616f3e56afc7e3d9c520b3df02b93b0fd
Damian0222 <d00229247@student.dkit.ie>
2025-02-09 00:42:25 +0000

    Changed fields, now it contains title, YearPublished, genre, PageCount, AthorID

--- S2CA1DamianMagiera/DTO/BookDto.cs
+++ S2CA1DamianMagiera/DTO/BookDto.cs
@@ -2,8 +2,10 @@
 {
     public class BookDTO
     {
-        public int Id { get; set; }
         public string Title { get; set; }
-        public int AuthorId { get; set; }
+        public int YearPublished { get; set; }
+        public string Genre { get; set; }
+        public int PageCount { get; set; }
+        public int AuthorId { get; set; } 
     }
 }
--- S2CA1DamianMagiera/Models/Book.cs
+++ S2CA1DamianMagiera/Models/Book.cs
@@ -4,6 +4,9 @@
     {
         public int Id { get; set; }
         public string Title { get; set; }
+        public int YearPublished { get; set; } 
+        public string Genre { get; set; }
+        public int PageCount { get; set; } 
         public int AuthorId { get; set; } 
     }
 }
