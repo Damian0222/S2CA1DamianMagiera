4640d4741d9f1a28b38ee7459bb63bf7f6b39ba7
Damian0222 <d00229247@student.dkit.ie>
2025-02-09 22:05:54 +0000

    Updated libraby class for a functional search method

--- S2CA1DamianMagiera/Data/Library.cs
+++ S2CA1DamianMagiera/Data/Library.cs
@@ -1,16 +1,25 @@
 ﻿
 using System.Collections.Generic;
+using Humanizer;
 using Microsoft.EntityFrameworkCore;
 using S2CA1DamianMagiera.Models;
 
 namespace S2CA1DamianMagiera.Data
 {
+    //Connects code to database
     public class Library : DbContext
     {
         public Library(DbContextOptions<Library> options) : base(options) { }
 
         public DbSet<Author> Authors { get; set; }
-    
-    public DbSet<Book> Books { get; set; }
+
+        public DbSet<Book> Books { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>()
+                .HasOne(b => b.Author)   // A book has one author
+                .WithMany(a => a.Books)  // An author has many books
+                .HasForeignKey(b => b.AuthorId); // Foreign key
+        }
     }
 }
